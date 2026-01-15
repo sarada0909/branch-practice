56088c3cc1ad2b41912012ba4adb01710d50ddbe
sarada0909 <sarada0909@gmail.com>
2026-01-15 11:45:13 +0900

    refactor: Do fizzbuzz with 1 if Statement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,6 @@
 for i in range(16, 30+1):
-    # FB on branch main
-    if i%15==0:
-        print('fizzbuzz')
-    elif i%3==0:
-        print('fizz')
-    elif i%5==0:
-        print('buzz')
+    # 1-line fizzbuzz on branch rebase-test
+    if i%3==0 or i%5==0:
+        print('fizz'*(i%3==0)+'buzz'*(i%5==0))
     else:
         print(i)
